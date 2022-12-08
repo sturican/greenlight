53515a99cf0d6de5ea2d422b33c23017480f86ac
Yuriy A. Trushkin <i@trushkin.ru>
2022-12-08 14:09:40 +0300

    fix(content): for mgsu custom test

--- config/application.rb
+++ config/application.rb
@@ -145,7 +145,7 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "https://raw.githubusercontent.com/bigbluebutton/greenlight/master/app/assets/images/logo_with_text.png"
+    config.branding_image_default = "https://mgsu.ru/logomgsu.svg"
 
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
